de2a74206edc9c63d987dec379ecbb0671e31d93
Vanessa Maldonado <vanessa.rodriguez3@mavs.uta.edu>
2022-07-16 21:09:42 -0500

    adding intern test

---
+++ __tests__/Manager.test.js
@@ -0,0 +1,14 @@
+// require the Manager constructor
+const Manager = require('../lib/Manager');
+
+// create a Manager object 
+test('create the Manager object', () => {
+    const manager = new Manager('Anthony', 1, 'anthony.test@gmail.com', '1109');
+    expect(manager.officeNumber).toEqual(expect.any(Number));
+});
+
+// getRole()
+test('get Manager role', () => {
+    const manager = new Manager('Anthony', 1, 'anthony.test@gmail.com', '1109');
+    expect(manager.getRole()).toEqual('Manager');
+});
